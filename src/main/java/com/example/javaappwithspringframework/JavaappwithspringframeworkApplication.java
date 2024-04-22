6ecee377cfa17da66dcb90dbb9d988154d7c94c5
bugaj55113 <bbugi2016@gmail.com>
2024-04-22 10:40:16 +0200

    Proj init

---
+++ src/main/java/com/example/javaappwithspringframework/JavaappwithspringframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaappwithspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaappwithspringframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaappwithspringframeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaappwithspringframework/JavaappwithspringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaappwithspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaappwithspringframeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
