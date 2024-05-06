50d7db51b8df59dea39a2adc7b9cdf933ed05cbf
bugaj55113 <bbugi2016@gmail.com>
2024-05-06 10:25:54 +0200

    Controller fix

--- src/main/java/com/example/javaappwithspringframework/controllers/DirectorController.java
+++ src/main/java/com/example/javaappwithspringframework/controllers/DirectorController.java
@@ -1,9 +1,11 @@
 package com.example.javaappwithspringframework.controllers;
 
 import com.example.javaappwithspringframework.repositories.DirectorRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class DirectorController {
     private DirectorRepository directorRepository;
 
@@ -13,7 +15,7 @@ public class DirectorController {
 
     @RequestMapping("/directors")
     public String getDirectors(Model model) {
-        model.addAttribute("direcotrs", directorRepository.findAll());
+        model.addAttribute("directors", directorRepository.findAll());
         return "directors";
     }
 }
