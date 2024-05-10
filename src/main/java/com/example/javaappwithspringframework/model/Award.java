07e0822c294ebb00cf68cd4424edca13ca8aa12b
bugaj55113 <bbugi2016@gmail.com>
2024-05-10 19:36:36 +0200

    Award model added

---
+++ src/main/java/com/example/javaappwithspringframework/model/Award.java
@@ -0,0 +1,79 @@
+package com.example.javaappwithspringframework.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Award {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String year;
+
+    @ManyToMany(mappedBy = "awards")
+    private Set<Director> directors = new HashSet<>();
+
+    @ManyToMany(mappedBy = "awards")
+    private Set<Actor> actors = new HashSet<>();
+
+    @ManyToMany(mappedBy = "awards")
+    private Set<Movie> movies = new HashSet<>();
+
+    public Award() {
+    }
+
+    public Award(String name, String year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Set<Director> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(Set<Director> directors) {
+        this.directors = directors;
+    }
+
+    public Set<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(Set<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+}
