983d63c50ab2bb9f31a912e22d5cfd4b0d79e198
bugaj55113 <bbugi2016@gmail.com>
2024-05-12 15:45:55 +0200

    New data init

--- src/main/java/com/example/javaappwithspringframework/tools/DBInflater.java
+++ src/main/java/com/example/javaappwithspringframework/tools/DBInflater.java
@@ -2,6 +2,10 @@ package com.example.javaappwithspringframework.tools;
 
 import com.example.javaappwithspringframework.model.Director;
 import com.example.javaappwithspringframework.model.Movie;
+import com.example.javaappwithspringframework.model.Actor;
+import com.example.javaappwithspringframework.model.Award;
+import com.example.javaappwithspringframework.repositories.ActorRepository;
+import com.example.javaappwithspringframework.repositories.AwardRepository;
 import com.example.javaappwithspringframework.repositories.DirectorRepository;
 import com.example.javaappwithspringframework.repositories.MovieRepository;
 import org.springframework.context.ApplicationListener;
@@ -10,13 +14,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DBInflater implements ApplicationListener<ContextRefreshedEvent> {
-    public DBInflater(MovieRepository movieRepository, DirectorRepository directorRepository) {
-        this.movieRepository = movieRepository;
-        this.directorRepository = directorRepository;
-    }
-
     private MovieRepository movieRepository;
     private DirectorRepository directorRepository;
+    private ActorRepository actorRepository;
+    private AwardRepository awardRepository;
+
+    public DBInflater(MovieRepository movieRepository, DirectorRepository directorRepository, ActorRepository actorRepository, AwardRepository awardRepository) {
+        this.movieRepository = movieRepository;
+        this.directorRepository = directorRepository;
+        this.actorRepository = actorRepository;
+        this.awardRepository = awardRepository;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -24,31 +32,51 @@ public class DBInflater implements ApplicationListener<ContextRefreshedEvent> {
     }
 
     private void initData() {
-
+        // First set of data
         Director fincher = new Director("David", "Fincher", "28-08-1962");
+        Actor pitt = new Actor("Brad", "Pitt", "18-12-1963");
+        Award oscar = new Award("Oscar for Best Actor", "1999");
         Movie fightClub = new Movie("Fight Club", "drama", "1999", "english", "USA");
-        fincher.getMovies().add(fightClub);
-        fightClub.getDirectors().add(fincher);
+
         directorRepository.save(fincher);
+        awardRepository.save(oscar);
+
+        fightClub.getDirectors().add(fincher);
+        fightClub.getActors().add(pitt);
+        pitt.getAwards().add(oscar);
+        actorRepository.save(pitt);
         movieRepository.save(fightClub);
 
 
-        Director bareja = new Director("Stanisław", "Bareja", "05-12-1929");
-        Movie zmiennicy = new Movie("Zmiennicy", "Komedia", "1986", "polish", "Poland");
-        bareja.getMovies().add(zmiennicy);
-        zmiennicy.getDirectors().add(bareja);
-        directorRepository.save(bareja);
-        movieRepository.save(zmiennicy);
+        Director nolan = new Director("Christopher", "Nolan", "30-07-1970");
+        Actor dicaprio = new Actor("Leonardo", "DiCaprio", "11-11-1974");
+        Award bafta = new Award("BAFTA for Best Film", "2010");
+        Movie inception = new Movie("Inception", "Sci-fi", "2010", "English", "USA");
+
+        directorRepository.save(nolan);
+        actorRepository.save(dicaprio);
+        awardRepository.save(bafta);
+
+        inception.getDirectors().add(nolan);
+        inception.getActors().add(dicaprio);
+        inception.getAwards().add(bafta);
+        movieRepository.save(inception);
 
 
+        Director spielberg = new Director("Steven", "Spielberg", "18-12-1946");
+        Actor goldblum = new Actor("Jeff", "Goldblum", "22-10-1952");
+        Award goldenGlobe = new Award("Golden Globe for Best Director", "1993");
+        Movie jurassicPark = new Movie("Jurassic Park", "Adventure", "1993", "English", "USA");
 
-        Director kurosawa = new Director("Akira", "Kurosawa", "23-03-1910");
-        Movie sevenSamurai = new Movie("Seven Samurai", "action", "1954", "japanese", "Japan");
-        kurosawa.getMovies().add(sevenSamurai);
-        fightClub.getDirectors().add(kurosawa);
-        directorRepository.save(kurosawa);
-        movieRepository.save(sevenSamurai);
+        actorRepository.save(goldblum);
+        awardRepository.save(goldenGlobe);
 
+        spielberg.getAwards().add(goldenGlobe);
+        directorRepository.save(spielberg);
+        jurassicPark.getDirectors().add(spielberg);
+        jurassicPark.getActors().add(goldblum);
+        movieRepository.save(jurassicPark);
     }
+
 
 }
