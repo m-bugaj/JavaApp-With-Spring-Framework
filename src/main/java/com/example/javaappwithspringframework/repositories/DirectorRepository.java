9b409791275ebb17fd5bb4e4cf41eb5da949cdff
bugaj55113 <bbugi2016@gmail.com>
2024-05-12 15:45:09 +0200

    Repository uppdated

--- src/main/java/com/example/javaappwithspringframework/repositories/DirectorRepository.java
+++ src/main/java/com/example/javaappwithspringframework/repositories/DirectorRepository.java
@@ -3,5 +3,10 @@ package com.example.javaappwithspringframework.repositories;
 import com.example.javaappwithspringframework.model.Director;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface DirectorRepository extends CrudRepository<Director, Long> {
+    Optional<Director> getFirstByFirstNameAndLastName(String firstName, String lastName);
+
+    Optional<Director> getFirstById(Long id);
 }
--- src/main/java/com/example/javaappwithspringframework/repositories/MovieRepository.java
+++ src/main/java/com/example/javaappwithspringframework/repositories/MovieRepository.java
@@ -3,5 +3,8 @@ package com.example.javaappwithspringframework.repositories;
 import com.example.javaappwithspringframework.model.Movie;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository  extends CrudRepository<Movie, Long> {
+    Optional<Movie> getFirstByTitleAndReleaseDate(String title, String releaseDate);
 }
