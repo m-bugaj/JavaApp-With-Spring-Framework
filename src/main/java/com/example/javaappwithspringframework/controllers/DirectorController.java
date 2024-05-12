3bedec6b53c84e88034e85c36ce0dda90b3702f7
bugaj55113 <bbugi2016@gmail.com>
2024-05-12 15:43:46 +0200

    CRUD implementation

--- src/main/java/com/example/javaappwithspringframework/controllers/DirectorController.java
+++ src/main/java/com/example/javaappwithspringframework/controllers/DirectorController.java
@@ -1,21 +1,103 @@
 package com.example.javaappwithspringframework.controllers;
 
+import com.example.javaappwithspringframework.commands.DirectorCommand;
+import com.example.javaappwithspringframework.model.Director;
 import com.example.javaappwithspringframework.repositories.DirectorRepository;
+import com.example.javaappwithspringframework.converters.DirectorCommandToDirectorConverter;
+import com.example.javaappwithspringframework.converters.DirectorToDirectorCommandConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 public class DirectorController {
     private DirectorRepository directorRepository;
+    private DirectorCommandToDirectorConverter directorCommandToDirector;
+    private DirectorToDirectorCommandConverter directorToDirectorCommand;
 
-    public DirectorController(DirectorRepository directorRepository) {
+    public DirectorController(DirectorRepository directorRepository, DirectorCommandToDirectorConverter directorCommandToDirector, DirectorToDirectorCommandConverter directorToDirectorCommand) {
+
         this.directorRepository = directorRepository;
+        this.directorCommandToDirector = directorCommandToDirector;
+        this.directorToDirectorCommand = directorToDirectorCommand;
     }
 
-    @RequestMapping("/directors")
+    @RequestMapping(value = {"/directors", "director/list"})
     public String getDirectors(Model model) {
         model.addAttribute("directors", directorRepository.findAll());
-        return "directors";
+        return "director/list";
     }
+
+    @RequestMapping("/director/{id}/show")
+    public String getDirectorDetails(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("director", directorRepository.findById(id).get());
+        return "director/show";
+    }
+
+    @GetMapping("/director/new")
+    public String newDirector(Model model){
+        model.addAttribute("director", new DirectorCommand());
+        return "director/addedit";
+    }
+
+    @GetMapping("/director/{id}/edit")
+    public String editDirector(@PathVariable Long id, Model model) {
+
+        Optional<Director> directorOptional = directorRepository.getFirstById(id);
+
+        if (directorOptional.isPresent()) {
+            Director director = directorOptional.get();
+            DirectorCommand directorCommand = directorToDirectorCommand.convert(director);
+            model.addAttribute("director", directorCommand);
+            return "director/addedit";
+        } else {
+            System.out.println("Director not found!");
+            return "directors";
+        }
+    }
+
+    @GetMapping("/director/{id}/delete")
+    public String deleteDirector(@PathVariable Long id) {
+        Optional<Director> directorOptional = directorRepository.findById(id);
+
+        if (directorOptional.isPresent()) {
+            Director directorToRemove = directorOptional.get();
+            // Usuń powiązania z filmami
+            directorToRemove.getMovies().forEach(movie -> movie.getDirectors().remove(directorToRemove));
+            directorToRemove.getMovies().clear();  // Clear the list of movies
+            directorRepository.save(directorToRemove);  // Update the movies before deleting the director
+            directorRepository.delete(directorToRemove);
+            return "redirect:/directors";
+        } else {
+            return "redirect:/directors";
+        }
+    }
+
+    @PostMapping("/director/")
+    public String saveOrUpdate(@ModelAttribute DirectorCommand command){
+
+        Optional<Director> directorOptional = directorRepository.getFirstByFirstNameAndLastName(command.getFirstName(), command.getLastName());
+        Optional<Director> directorById = directorRepository.getFirstById(command.getId());
+
+        if (directorById.isPresent()){
+            Director editedDirector = directorById.get();
+            editedDirector.setFirstName(command.getFirstName());
+            editedDirector.setLastName(command.getLastName());
+            editedDirector.setDateOfBirth(command.getDateOfBirth());
+            directorRepository.save(editedDirector);
+            return "redirect:/director/" + editedDirector.getId() + "/show";
+        } else if (directorOptional.isEmpty()) {
+            Director detachedDirector = directorCommandToDirector.convert(command);
+            Director savedDirector = directorRepository.save(detachedDirector);
+            return "redirect:/director/" + savedDirector.getId() + "/show";
+        } else {
+            //TODO: error message to template
+            System.out.println("Sorry, there's such artist in db");
+            return "redirect:/director/" + directorOptional.get().getId() + "/show";
+        }
+    }
+
 }
--- src/main/java/com/example/javaappwithspringframework/controllers/MovieController.java
+++ src/main/java/com/example/javaappwithspringframework/controllers/MovieController.java
@@ -1,21 +1,102 @@
 package com.example.javaappwithspringframework.controllers;
 
+import com.example.javaappwithspringframework.commands.MovieCommand;
+import com.example.javaappwithspringframework.model.Movie;
 import com.example.javaappwithspringframework.repositories.MovieRepository;
+import com.example.javaappwithspringframework.converters.MovieCommandToMovieConverter;
+import com.example.javaappwithspringframework.converters.MovieToMovieCommandConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class MovieController {
     private MovieRepository movieRepository;
+    private MovieCommandToMovieConverter movieCommandToMovie;
+    private MovieToMovieCommandConverter movieToMovieCommand;
 
-    public MovieController(MovieRepository movieRepository) {
+    public MovieController(MovieRepository movieRepository, MovieCommandToMovieConverter movieCommandToMovie, MovieToMovieCommandConverter movieToMovieCommand) {
         this.movieRepository = movieRepository;
+        this.movieCommandToMovie = movieCommandToMovie;
+        this.movieToMovieCommand = movieToMovieCommand;
     }
 
-    @RequestMapping("/movies")
+    @RequestMapping(value = {"/movies", "movie/list"})
     public String getMovies(Model model) {
         model.addAttribute("movies", movieRepository.findAll());
-        return "movies";
+        return "movie/list";
     }
+
+    @RequestMapping("/movie/{id}/show")
+    public String getMovieDetails(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("movie", movieRepository.findById(id).orElse(null));
+        return "movie/show";
+    }
+
+    @GetMapping("/movie/new")
+    public String newMovie(Model model){
+        model.addAttribute("movie", new MovieCommand());
+        return "movie/addedit";
+    }
+
+    @GetMapping("/movie/{id}/edit")
+    public String editMovie(@PathVariable Long id, Model model) {
+        Optional<Movie> movieOptional = movieRepository.findById(id);
+        if (movieOptional.isPresent()) {
+            Movie movie = movieOptional.get();
+            MovieCommand movieCommand = movieToMovieCommand.convert(movie);
+            model.addAttribute("movie", movieCommand);
+            return "movie/addedit";
+        } else {
+            System.out.println("Movie not found!");
+            return "redirect:/movies";
+        }
+    }
+
+    @GetMapping("/movie/{id}/delete")
+    public String deleteMovie(@PathVariable Long id) {
+        Optional<Movie> movieOptional = movieRepository.findById(id);
+
+        if (movieOptional.isPresent()) {
+            Movie movieToRemove = movieOptional.get();
+            // Usuń powiązania z reżyserami i aktorami
+            movieToRemove.getDirectors().clear();
+            movieToRemove.getActors().clear();
+            movieToRemove.getAwards().clear();  // Clearing related entities
+            movieRepository.save(movieToRemove);  // Save changes before deleting movie
+            movieRepository.delete(movieToRemove);
+            return "redirect:/movies";
+        } else {
+            return "redirect:/movies";
+        }
+    }
+
+
+    @PostMapping("/movie/")
+    public String saveOrUpdate(@ModelAttribute MovieCommand command) {
+        Optional<Movie> movieById = movieRepository.findById(command.getId());
+        Optional<Movie> movieByTitleAndDate = movieRepository.getFirstByTitleAndReleaseDate(command.getTitle(), command.getReleaseDate());
+
+        if (movieById.isPresent()) {
+            Movie editedMovie = movieById.get();
+            editedMovie.setTitle(command.getTitle());
+            editedMovie.setGenre(command.getGenre());
+            editedMovie.setReleaseDate(command.getReleaseDate());
+            editedMovie.setLanguage(command.getLanguage());
+            editedMovie.setCountry(command.getCountry());
+            movieRepository.save(editedMovie);
+            return "redirect:/movie/" + editedMovie.getId() + "/show";
+        } else if (movieByTitleAndDate.isEmpty()) {
+            Movie newMovie = movieCommandToMovie.convert(command);
+            Movie savedMovie = movieRepository.save(newMovie);
+            return "redirect:/movie/" + savedMovie.getId() + "/show";
+        } else {
+            System.out.println("Sorry, there's such movie in db");
+            return "redirect:/movie/" + movieByTitleAndDate.get().getId() + "/show";
+        }
+    }
+
+
 }
