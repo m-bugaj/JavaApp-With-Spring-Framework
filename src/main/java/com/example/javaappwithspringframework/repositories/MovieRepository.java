dd81160e29bb42fab0f8c4afb2c31ab353ea6fef
bugaj55113 <bbugi2016@gmail.com>
2024-05-20 14:31:50 +0200

    Updated movie repository

--- src/main/java/com/example/javaappwithspringframework/repositories/MovieRepository.java
+++ src/main/java/com/example/javaappwithspringframework/repositories/MovieRepository.java
@@ -1,10 +1,12 @@
 package com.example.javaappwithspringframework.repositories;
 
+import com.example.javaappwithspringframework.model.Director;
 import com.example.javaappwithspringframework.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface MovieRepository  extends CrudRepository<Movie, Long> {
+public interface MovieRepository  extends JpaRepository<Movie, Long> {
     Optional<Movie> getFirstByTitleAndReleaseDate(String title, String releaseDate);
 }
