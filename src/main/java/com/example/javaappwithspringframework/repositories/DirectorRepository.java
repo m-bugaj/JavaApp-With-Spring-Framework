90fc6be6e302d4fa0f6c5dc446b655bfbce1aec8
bugaj55113 <bbugi2016@gmail.com>
2024-05-29 17:04:46 +0200

    Updated director repository

--- src/main/java/com/example/javaappwithspringframework/repositories/DirectorRepository.java
+++ src/main/java/com/example/javaappwithspringframework/repositories/DirectorRepository.java
@@ -1,12 +1,16 @@
 package com.example.javaappwithspringframework.repositories;
 
 import com.example.javaappwithspringframework.model.Director;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface DirectorRepository extends CrudRepository<Director, Long> {
+public interface DirectorRepository extends JpaRepository<Director, Long> {
     Optional<Director> getFirstByFirstNameAndLastName(String firstName, String lastName);
 
     Optional<Director> getFirstById(Long id);
+
+    List<Director> getByFirstName(String firstName);
 }
